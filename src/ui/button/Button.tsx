5633bea4fbc6a452a6095d35c869ec152dec8041
Sofya Kalmykova <Sofkakalmykova@yandex.ru>
2026-09-13 21:56:14 +0300

    Работа на проверку

--- src/ui/button/Button.tsx
+++ src/ui/button/Button.tsx
@@ -2,6 +2,7 @@ import { Text } from 'src/ui/text';
 
 import styles from './Button.module.scss';
 import { clsx } from 'clsx';
+import React from 'react';
 
 export const Button = ({
 	title,
@@ -10,7 +11,7 @@ export const Button = ({
 	type,
 }: {
 	title: string;
-	onClick?: () => void;
+	onClick?: (e: React.FormEvent) => void;
 	htmlType?: React.ButtonHTMLAttributes<HTMLButtonElement>['type'];
 	type: 'apply' | 'clear';
 }) => {
